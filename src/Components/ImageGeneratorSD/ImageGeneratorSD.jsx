d82bff64042757e659f2c067d044b23532c9acab
MC <mc@MCs-MacBook-Pro.attlocal.net>
2023-12-30 04:03:50 -0800

    new stable diffusion

--- src/Components/ImageGeneratorSD/ImageGeneratorSD.jsx
+++ src/Components/ImageGeneratorSD/ImageGeneratorSD.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+// ImageGeneratorSD.js
+import React, { useEffect } from 'react';
 import Iframe from 'react-iframe';
 import './ImageGeneratorSD.css';
 
 const ImageGeneratorSD = () => {
+  useEffect(() => {
+    const iframe = document.getElementById('myId');
+    const overlayDiv = document.createElement('div');
+
+    overlayDiv.classList.add('overlay');
+    iframe.parentNode.insertBefore(overlayDiv, iframe);
+
+    return () => {
+      // Clean up the overlay when the component unmounts
+      if (overlayDiv.parentNode) {
+        overlayDiv.parentNode.removeChild(overlayDiv);
+      }
+    };
+  }, []);
+
   return (
     <div className="image-generator-sd">
       <h1>AI Image Generator SD</h1>
-      <Iframe
-        url="https://prodia-fast-stable-diffusion.hf.space/"
-        width="100%"
-        height="600px"
-        id="myId"
-        className="myClassname"
-        display="initial"
-        position="relative"
-        allowFullScreen
-      />
+      <div className="iframe-container">
+        <Iframe
+          url="https://app.prodia.com"
+          width="100%"
+          height="600px"
+          id="myId"
+          className="myClassname"
+          display="initial"
+          position="relative"
+          allowFullScreen
+        />
+      </div>
     </div>
   );
 };
