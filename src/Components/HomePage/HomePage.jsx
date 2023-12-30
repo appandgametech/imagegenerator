a3b3aeb37925640b0bdc6bb79be9bcf4484fcf65
MC <mc@MCs-MacBook-Pro.attlocal.net>
2023-12-29 22:21:59 -0800

    adding update

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,4 +1,3 @@
-// HomePage.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './HomePage.css';
@@ -6,9 +5,11 @@ import './HomePage.css';
 const HomePage = () => {
   return (
     <div className="home-page">
-  <h1>
-  Welcome to AI Image Generator <span className="by-text">by</span> MUZZY GAMES
-</h1>
+      <h1>
+        Welcome to AI Image Generator <br />
+        <span className="by-text">by</span> Muzzy Games
+      </h1>
+
       <p>
         Explore our AI-powered image generators to create unique and fascinating images.
         Choose from different models and discover the creative possibilities.
@@ -17,7 +18,7 @@ const HomePage = () => {
         <img
           src="https://news.ubc.ca/wp-content/uploads/2023/08/AdobeStock_559145847.jpeg"
           alt=""
-          style={{ maxWidth: '100%', height: 'auto' }}
+          style={{ width: '60%', height: 'auto' }}
         />
       </div>
       <h2>Select an option below:</h2>
